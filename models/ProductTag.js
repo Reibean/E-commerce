aa1849faa2df20d70fc5297dce8be735ba0cd759
Reiley Burnitt <burnittreiley@gmail.com>
2023-10-06 15:45:44 -0600

    MVP finished for submission

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -13,7 +13,7 @@ ProductTag.init(
       autoIncrement: true,
     },
     product_id: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
       allowNull: false,
       references: {
         model: 'product',
